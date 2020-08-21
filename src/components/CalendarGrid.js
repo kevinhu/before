0ef9d8178672eadc2b1c3b59db4ad5b9d1b86460
Kevin Hu <kevinhuwest@gmail.com>
2020-08-21 00:29:50 -0400

    track selected date in state

--- src/components/CalendarGrid.js
+++ src/components/CalendarGrid.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import moment from 'moment';
 
 function CalendarGrid() {
-  var enumerateDaysBetweenDates = function (startDate, endDate) {
-    var dates = [];
+  const [selectedDate, setSelectedDate] = useState();
 
-    var currDate = moment(startDate).startOf('day');
-    var lastDate = moment(endDate).startOf('day');
+  let enumerateDaysBetweenDates = function (startDate, endDate) {
+    let dates = [];
+
+    let currDate = moment(startDate).startOf('day');
+    let lastDate = moment(endDate).startOf('day');
 
     while (currDate.add(1, 'days').diff(lastDate) < 0) {
       dates.push(currDate.clone());
@@ -17,15 +19,15 @@ function CalendarGrid() {
 
   const DATE_FORMAT = 'YYYYMMDD';
 
-  var startDate = moment('20130101', DATE_FORMAT);
-  var endDate = moment('20140101', DATE_FORMAT);
+  let startDate = moment('20130101', DATE_FORMAT);
+  let endDate = moment('20140101', DATE_FORMAT);
 
-  var dates = enumerateDaysBetweenDates(startDate, endDate);
+  let dates = enumerateDaysBetweenDates(startDate, endDate);
 
-  var datesByWeek = [];
+  let datesByWeek = [];
 
-  var week_ = 1;
-  var week_group = [];
+  let week_ = 1;
+  let week_group = [];
 
   for (const date of dates) {
     if (week_ !== date.week()) {
@@ -37,33 +39,53 @@ function CalendarGrid() {
     week_group.push(date);
   }
 
-  if(week_group.length > 0){
+  if (week_group.length > 0) {
     datesByWeek.push(week_group);
   }
 
-  var firstWeek = datesByWeek[0]
+  const firstWeek = datesByWeek[0];
 
-  while(firstWeek.length < 7){
-    firstWeek.unshift(firstWeek[0].clone().add(-1,'days'))
+  while (firstWeek.length < 7) {
+    firstWeek.unshift(firstWeek[0].clone().add(-1, 'days'));
   }
 
-  var lastWeek = datesByWeek.slice(-1)[0] 
+  const lastWeek = datesByWeek.slice(-1)[0];
 
-  while(lastWeek.length < 7){
-    lastWeek.push(lastWeek.slice(-1)[0].clone().add(1,'days'))
+  while (lastWeek.length < 7) {
+    lastWeek.push(lastWeek.slice(-1)[0].clone().add(1, 'days'));
   }
+
+  const GridMouseEnter = (event) => {
+    const date = moment(parseInt(event.target.dataset.date));
+
+    setSelectedDate(date);
+  };
+
+  const GridMouseLeave = (event) => {
+    // console.log(event.target.dataset)
+  };
 
   return (
-    <div className="flex p-2 bg-gray-200 rounded-lg shadow-inner" style={{width:"max-content",margin:"0 auto"}}>
-      {datesByWeek.map((week) => (
-        <div className={`block`}>
-          {week.map((date) => (
-            <div
-              className={`block bg-green-500 w-4 h-4 rounded text-xs`}
-              style={{ margin: '2px' }}></div>
-          ))}
-        </div>
-      ))}
+    <div>
+      <div
+        className="flex p-2 bg-gray-200 rounded-lg shadow-inner"
+        style={{ width: 'max-content', margin: '0 auto' }}>
+        {datesByWeek.map((week) => (
+          <div className={`block`}>
+            {week.map((date) => (
+              <div
+                className={`block bg-green-500 w-4 h-4 rounded text-xs cursor-pointer`}
+                style={{ margin: '2px' }}
+                data-date={date}
+                onMouseEnter={GridMouseEnter}
+                onMouseLeave={GridMouseLeave}></div>
+            ))}
+          </div>
+        ))}
+      </div>
+      <div className="text-center mt-12">
+        Top pages on {selectedDate && selectedDate.format('L')}{' '}
+      </div>
     </div>
   );
 }
