f932e34d9315822fb6a4cb219c75551af1cc0330
Kevin Hu <kevinhuwest@gmail.com>
2020-08-21 23:24:46 -0400

    hackernews json

--- scraper/3a_aggregate_hackernews.py
+++ scraper/3a_aggregate_hackernews.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import config
+import ujson
 
 # limit top n per day
 DAILY_LIMIT = 16
@@ -13,19 +14,33 @@ hackernews = hackernews[~hackernews["points"].isna()]
 hackernews = hackernews[hackernews["url"].apply(lambda x: "github.com" in x)]
 
 # group by day
-hackernews["date"] = pd.to_datetime(hackernews["created_at_i"],unit='s')
-hackernews["day"] = hackernews["date"].dt.floor('d')
-hackernews = hackernews.sort_values(by=["day","points"],ascending=[True,False])
+hackernews["date"] = pd.to_datetime(hackernews["created_at_i"], unit="s")
+hackernews["day"] = hackernews["date"].dt.floor("d").astype(str)
+hackernews = hackernews.sort_values(by=["day", "points"], ascending=[True, False])
 
 # filter daily ranks
-hackernews["daily_rank"] = hackernews.groupby("day")["points"].rank(method="first",ascending=False)
+hackernews["daily_rank"] = hackernews.groupby("day")["points"].rank(
+    method="first", ascending=False
+)
 hackernews = hackernews[hackernews["daily_rank"] <= DAILY_LIMIT]
 
 # reset index
-hackernews.reset_index(inplace=True,drop=True)
+hackernews.reset_index(inplace=True, drop=True)
+
+# drop some columns
+hackernews = hackernews[
+    ["title", "url", "author", "points", "date", "day", "daily_rank"]
+]
 
 hackernews.to_feather(
     config.DATA_DIR / "processed/hackernews_github.feather",
     compression=config.FEATHER_COMPRESSION,
     compression_level=config.FEATHER_COMPRESSION_LEVEL,
 )
+
+hackernews["date"] = hackernews["date"].astype(str)
+hackernews_dict = hackernews.to_dict("records")
+hackernews_dict = {x["day"]: x for x in hackernews_dict}
+
+with open(config.DATA_DIR / "processed/hackernews_github.json", "w") as f:
+	f.write(ujson.dumps(hackernews_dict))
