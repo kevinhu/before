13e40e6e98beec0e911108d9400048a66bbb4c16
Kevin Hu <kevinhuwest@gmail.com>
2020-08-23 18:03:13 -0400

    Updates

---
+++ precache-manifest.7106a1fd09e0e38dbc02b819acfbd992.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "787105b0bf72bfa9016028ad7b7d4ed9",
+    "url": "/before/index.html"
+  },
+  {
+    "revision": "b2cc895954ca9e7806a4",
+    "url": "/before/static/css/main.0fc2b200.chunk.css"
+  },
+  {
+    "revision": "aef697438a9b72c8e79c",
+    "url": "/before/static/js/2.fd2b37d3.chunk.js"
+  },
+  {
+    "revision": "270af70aba11570f7199ae7f2b5cf855",
+    "url": "/before/static/js/2.fd2b37d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2cc895954ca9e7806a4",
+    "url": "/before/static/js/main.b412de52.chunk.js"
+  },
+  {
+    "revision": "7bdcb6b1d583df9a8793",
+    "url": "/before/static/js/runtime-main.be5975d3.js"
+  },
+  {
+    "revision": "49a9f44e6d72b46ac5d47fc63be46001",
+    "url": "/before/static/media/cat-in-space.49a9f44e.svg"
+  },
+  {
+    "revision": "bd257a4fd8e20bec845caf3f99ddaf34",
+    "url": "/before/static/media/hackernews_icon.bd257a4f.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/before/precache-manifest.ec5b8169244f024960d3d5ed90eac816.js"
+  "/before/precache-manifest.7106a1fd09e0e38dbc02b819acfbd992.js"
 );
 
 self.addEventListener('message', (event) => {
